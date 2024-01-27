ba4a0ee640e2c59d05e5a3c46ee0c130a7d9c36d
Jubayer-Ahmed-Sajid <jubayerahmed661ac@gmail.com>
2024-01-27 11:17:16 +0600

    [feat](jwt-implementation):token generated and verified

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const jwt = require('jsonwebtoken');
 // middleware
 app.use(cookieParser());
 app.use(cors({
-  origin:['https://fitness-studio-8a6af.web.app'],
-  credentials:true,
-  
+  origin: ['http://localhost:5173'],
+  credentials: true,
+
 }));
 app.use(express.json());
 
@@ -27,6 +27,25 @@ const client = new MongoClient(uri, {
   },
 });
 
+// verify token middleware
+
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies.token
+  jwt.verify(token, process.env.SECRET_TOKEN, function (err, decoded) {
+    if (!token) {
+      return res.status(401).send({ message: 'unauthorized' })
+    }
+    if (err) {
+      return res.status(403).send({ message: 'Bad Request' })
+    }
+    else {
+      console.log('decoded code', decoded)
+      req.user = decoded
+      next()
+    }
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -37,27 +56,31 @@ async function run() {
 
     // feedback start
 
-    app.get("/feedback", async (req, res) => {
+    app.get("/feedback", verifyToken, async (req, res) => {
       const result = await FeedbackCollection.find().toArray();
       res.send(result);
     });
 
     // feedback end
 
-    // Auth related api
+    // Auth related api start
     app.post('/jwt', async (req, res) => {
       const user = req.body
       console.log(user)
       const token = jwt.sign(user, process.env.SECRET_TOKEN, { expiresIn: '1h' })
       console.log('token is', token)
       res.
-      cookie("token", token, 
-      { httpOnly: true, 
-        secure: false, 
-        sameSite: 'none' 
-      })
-      .send({ setToken: 'success' })
+        cookie("token", token,
+          {
+            httpOnly: true,
+            secure: false,
+            sameSite: 'Lax'
+          })
+        .send({ setToken: 'success' })
     })
+
+        // Auth related api end
+
 
     // user start
     app.post("/users", async (req, res) => {
@@ -70,39 +93,58 @@ async function run() {
       const result = await UsersCollection.insertOne(user);
       res.send(result);
     });
-    app.post("/user_goal", async (req, res) => {
+
+
+    app.post("/user_goal", verifyToken, async (req, res) => {
       const goalInfo = req.body;
-      const token = console.log('tok tok token', req.cookies.token)
       const result = await UserGoalCollection.insertOne(goalInfo);
       res.send(result);
     });
 
-    app.get("/users", async (req, res) => {
+
+    app.get("/users", verifyToken, async (req, res) => {
       const result = await UsersCollection.find().toArray();
       res.send(result);
     });
-    app.get("/users/:email", async (req, res) => {
+
+
+    app.get("/users/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
-      const query = { email: email };
-      const result = await UsersCollection.findOne(query);
-      res.send(result);
+      if (email !== req.user.email) {
+        return res.status(403).send({ message: 'forbidden' })
+      }
+      else {
+
+        const query = { email: email };
+        const result = await UsersCollection.findOne(query);
+        res.send(result);
+      }
     });
-    app.put("/update_user_data/:email", async (req, res) => {
+
+
+    app.put("/update_user_data/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
       const data = req?.body;
-      const query = { email: email };
-      console.log(data);
-      const updatedData = {
-        $set: {
-          name: data?.name,
-          birthDay: data?.birthDay,
-          weight: data?.weight,
-          height: data?.height,
-          gender: data?.gender,
-        },
-      };
-      const result = await UsersCollection.updateOne(query, updatedData);
-      res.send(result);
+      if (email !== req?.user?.email) {
+        return res.status(403).send({ message: 'forbidden' })
+
+      }
+      else {
+
+        const query = { email: email };
+        console.log(data);
+        const updatedData = {
+          $set: {
+            name: data?.name,
+            birthDay: data?.birthDay,
+            weight: data?.weight,
+            height: data?.height,
+            gender: data?.gender,
+          },
+        };
+        const result = await UsersCollection.updateOne(query, updatedData);
+        res.send(result);
+      }
     });
 
     // user end
