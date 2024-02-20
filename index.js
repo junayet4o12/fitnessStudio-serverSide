3d56ac6f00aa1a09af829605dae1fb1d225fc57e
Barkat ullah <barkatullah585464@gmail.com>
2024-02-20 13:29:09 +0600

    set the user_goal delete method

--- index.js
+++ index.js
@@ -474,6 +474,7 @@ async function run() {
 
     app.delete("/delete_blog/:id", async (req, res) => {
       const id = req?.params?.id;
+      console.log(id);
       const query = { _id: new ObjectId(id) };
       const result = await BlogsCollection.deleteOne(query);
       res.send(result);
