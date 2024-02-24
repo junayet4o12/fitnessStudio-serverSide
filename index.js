919337e6560b927b954b79ed47c2ddc82f7e2e74
Md-Arman01 <137868415+Md-Arman01@users.noreply.github.com>
2024-02-25 01:05:18 +0600

    [feat](event): delete specific event form database

--- index.js
+++ index.js
@@ -785,6 +785,12 @@ async function run() {
       const result = await EventsBookingCollection.deleteOne(query);
       res.send(result);
     });
+    app.delete("/all_event/:id", async (req, res) => {
+      const id = req?.params?.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await EventsCollection.deleteOne(query);
+      res.send(result);
+    });
     
 
     // event api end
